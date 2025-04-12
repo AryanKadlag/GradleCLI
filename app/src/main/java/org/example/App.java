d880257992fe43314db84d6a13ee49ea03626147
AryanKadlag <aryankadlag20@gmail.com>
2025-04-12 23:38:16 +0530

    Added okhttp to make http requests from plain java code

--- app/src/main/java/org/example/App.java
+++ app/src/main/java/org/example/App.java
@@ -3,12 +3,34 @@
  */
 package org.example;
 
+import okhttp3.OkHttp;
+import okhttp3.OkHttpClient;
+import okhttp3.Request;
+import okhttp3.Response;
+
+import java.io.IOException;
+
 public class App {
-    public String getGreeting() {
-        return "Hello World!";
-    }
 
     public static void main(String[] args) {
-        System.out.println(new App().getGreeting());
+
+        OkHttpClient client = new OkHttpClient();
+
+        String url = "https://jsonplaceholder.typicode.com/todos/1";
+
+        Request request = new Request.Builder()
+                                .url(url)
+                                .build();
+
+        try(Response response = client.newCall(request).execute()) {
+            if(!response.isSuccessful()){
+                System.out.println("Something Went Wrong");
+            }
+            System.out.println(response.body().string());
+
+        } catch (IOException ex) {
+            ex.printStackTrace();
+        }
+
     }
 }
